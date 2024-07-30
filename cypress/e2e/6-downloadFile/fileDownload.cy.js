ab83d068722cce28f5db4608c80cb1f85035fdd0
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-07-29 23:04:34 -0400

    add

--- cypress/e2e/6-downloadFile/fileDownload.cy.js
+++ cypress/e2e/6-downloadFile/fileDownload.cy.js
@@ -8,7 +8,7 @@ describe('verify file download',function(){
         cy.readFile('cypress/downloads/sample2.csv', {timeout: 60000}).should('contain','Eldon Base') 
     })
 
-    it.only('verify download file by plugin',function(){
+    it('verify download file by plugin',function(){
         cy.visit('http://autopract.com/selenium/download.html')
         cy.get('#download').click()
         cy.verifyDownload('sample2.csv') //only file name requires not path
