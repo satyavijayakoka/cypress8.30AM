d82128ac4e433cc5e53d4422063241b9b5a7a3a0
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-08-02 14:41:04 -0400

    august2

---
+++ cypress/e2e/9-intercept/intercept1.cy.js
@@ -0,0 +1,20 @@
+///<reference types="cypress" />
+
+describe('verify intercept concept in cypress',function(){
+    it('verify get request',function(){
+        cy.visit('https://example.cypress.io/commands/network-requests')
+
+        cy.intercept({
+            url:'https://jsonplaceholder.cypress.io/comments/1',
+            method:'GET'
+        }).as('getComment')
+
+        cy.contains('Get Comment').click()
+        cy.wait('@getComment').then((res)=>{
+         // cy.log(res.response.body.body)
+         cy.get('[class="network-comment"]').should('have.text',res.response.body.body)
+        })
+        cy.get('[class="network-comment"]').should('contain','laudantium enim')
+
+    })
+})
