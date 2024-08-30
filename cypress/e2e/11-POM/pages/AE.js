659bb225e3b9883cd85ced2861ecd9806fcc4761
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-08-30 13:52:13 -0400

    add

--- cypress/e2e/11-POM/pages/AE.js
+++ cypress/e2e/11-POM/pages/AE.js
@@ -25,7 +25,19 @@ export default class AEPage{
         city : '[data-qa="city"]',
         zipcode : '[data-qa="zipcode"]',
         mobNo : '[data-qa="mobile_number"]',
-        creatAccBtn : '[data-qa="create-account"]'
+        creatAccBtn : '[data-qa="create-account"]',
+
+        // validation page
+        accCreatedMsg : '[data-qa="account-created"] > b',
+        continueBtn : '[data-qa="continue-button"]',
+
+        // logout
+        logout : 'a[href="/logout"]',
+
+        //  login
+        loginEmail : '[data-qa="login-email"]',
+        loginPw : '[data-qa="login-password"]',
+        loginBtn : '[data-qa="login-button"]'
 
     }
 
@@ -38,5 +50,41 @@ export default class AEPage{
       cy.get(this.selector.signUpBtn).click()
 
     }
+    // createUser fill data
+    createUser(el){
+      if(el.gender == "Mrs."){
+        cy.get(this.selector.mrs).click()
+      }
+      else if(el.gender == "Mr."){
+        cy.get(this.selector.mr).click()
+      }
+
+      cy.get(this.selector.pw).type(el.password)
+      cy.get(this.selector.Bday).type(el.dayB)
+      cy.get(this.selector.Bmonth).type(el.month)
+      cy.get(this.selector.Byear).type(el.year)
+      cy.get(this.selector.cBox1).click()
+      cy.get(this.selector.cBox2).click()
+      cy.get(this.selector.firstName).type(el.fn)
+      cy.get(this.selector.lastName).type(el.ln)
+      cy.get(this.selector.company).type(el.comny)
+      cy.get(this.selector.address1).type(el.add1)
+      cy.get(this.selector.address2).type(el.add2)
+      cy.get(this.selector.country).type(el.country)
+      cy.get(this.selector.state).type(el.state)
+      cy.get(this.selector.city).type(el.city)
+      cy.get(this.selector.zipcode).type(el.zipcode)
+      cy.get(this.selector.mobNo).type(el.mobNo)
+      cy.get(this.selector.creatAccBtn).click()
+    }
+
+    validation(css,msg){
+      cy.get(css).should('have.text',msg)
+    }
+
+    btnClick(btncss){
+      cy.get(btncss).click()
+    }
+
 
 }
--- cypress/fixtures/payloadAE.js
+++ cypress/fixtures/payloadAE.js
@@ -5,5 +5,16 @@ export default{
     password :'satya123',
     dayB : 15,
     month : 'August',
-    year :"1981"
+    year : "1981",
+    fn : "satya",
+    ln : 'koka',
+    comny : 'Infosys',
+    add1 : 'Hyderabad1',
+    add2 : 'Hyderabad2',
+    country : 'India',
+    state : 'Telangana',
+    city : 'Hyderabad',
+    zipcode : '520011',
+    mobno : 9666477714,
+
 }
